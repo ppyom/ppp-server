cd58be715131e8ef0a979e8f33ac216b76f6c1a7
ppyom <ouob77@kakao.com>
2024-09-30 09:32:08 +0900

    chore: 에러 처리 로직 추가

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.get('/', (req, res, next) => {
   return res.send({ github: 'https://github.com/ppp-server' });
 });
 
+app.use((req, res) => {
+  return res.status(404).send({ message: '찾을 수 없는 페이지입니다.' });
+});
+
+app.use((error, req, res, next) => {
+  console.error(error.stack);
+  return res.status(500).send({
+    message: '서버에서 오류가 발생했습니다.',
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 app.listen(config.host.port, () => {
   console.log('listen!');
 });
